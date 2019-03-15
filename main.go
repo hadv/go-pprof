6eabc27fbd0fcaf920026f46c96b6f0029a23b19
hadv <dvietha@gmail.com>
2019-03-15 23:00:54 +0700

    reformat code

--- main.go
+++ main.go
@@ -1,26 +1,27 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"time"
-	"math/rand"
-	"log"
 	"flag"
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
 	"runtime/pprof"
+	"time"
 )
+
 var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
 
 func main() {
-    flag.Parse()
-    if *cpuprofile != "" {
-        f, err := os.Create(*cpuprofile)
-        if err != nil {
-            log.Fatal(err)
+	flag.Parse()
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal(err)
 		}
 		defer f.Close()
-        if err := pprof.StartCPUProfile(f); err != nil {
-            log.Fatal("could not start CPU profile: ", err)
+		if err := pprof.StartCPUProfile(f); err != nil {
+			log.Fatal("could not start CPU profile: ", err)
 		}
 		defer pprof.StopCPUProfile()
 	}
@@ -31,7 +32,7 @@ func main() {
 		num = rand.Intn(68720001023)
 		fmt.Println(num)
 
-		if num % 20001023 == 0 {
+		if num%20001023 == 0 {
 			break
 		}
 	}
